4383cd0f36d3856b2d2f95871758d0645abe2c18
Davide Ciulla <davide.ciulla@hotmail.com>
2022-01-26 14:16:56 +0100

    Copied all components from svelte-reveal repl env

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,20 @@
 <script>
-	export let name;
+	/**
+	 * Hello visitor!
+	 *
+	 * My name is Dave and I'm the maintainer of svelte-reveal (https://github.com/DaveKeehl/svelte-reveal).
+	 * svelte-reveal is currently in beta, meaning that it has reached an MVP (all the basic functionalities are 
+	 * done and tested) but it still needs some work and refining.
+	 * 
+	 * If you notice some weird or unexpected behavior, please reach out by creating an issue
+	 * (https://github.com/DaveKeehl/svelte-reveal/issues/new/choose) on GitHub.
+	 *
+	 * DOCS: https://github.com/DaveKeehl/svelte-reveal#readme
+	 *
+	 * Have a good day!
+	 */
+	
+	import Demo from './Demo.svelte'
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<Demo />
